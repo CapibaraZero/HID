87ef53dbadea3b51c0794bd5c70ba9d204308337
andreock <vivanco11.ac@gmail.com>
2024-03-28 19:08:51 +0100

    Format and initialize HID with begin()

--- example/bluetooth.cpp
+++ example/bluetooth.cpp
@@ -3,21 +3,21 @@
 
 void setup()
 {
-  Serial0.begin(115200);
+  Serial.begin(115200);
   BLEHid ble_hid = BLEHid();
 
   for (;;)
   {
     if (ble_hid.is_device_connected())
     {
-      Serial0.println("Device connected. Typing in progress");
+      Serial.println("Device connected. Typing in progress");
       ble_hid.get_keyboard().type("Hello world!");
       ble_hid.get_mouse().move(200, 200, 4, 4);
       ble_hid.get_mouse().click();
     }
     else
     {
-      Serial0.println("Device not connected");
+      Serial.println("Device not connected");
     }
     delay(10000);
   }
--- usb_hid/USBHid.hpp
+++ usb_hid/USBHid.hpp
@@ -1,6 +1,6 @@
 /*
- * This file is part of the Capibara zero project(https://capibarazero.github.io/).
- * Copyright (c) 2023 Andrea Canale.
+ * This file is part of the Capibara zero
+ * project(https://capibarazero.github.io/). Copyright (c) 2023 Andrea Canale.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -21,30 +21,32 @@
 #include "USB.h"
 #include "USBHIDKeyboard.h"
 #include "USBHIDMouse.h"
+#include "Arduino.h"
 
-class USBHid
-{
-private:
-  USBHIDKeyboard Keyboard;
-  USBHIDMouse Mouse;
+class USBHid {
+   private:
+    USBHIDKeyboard Keyboard;
+    USBHIDMouse Mouse;
 
-public:
-  USBHid(/* args */);
-  ~USBHid();
+   public:
+    USBHid(/* args */);
+    ~USBHid();
 
-  void print_char(char char_to_print);
+    void print_char(char char_to_print);
 
-  void print_string(std::string string_to_print);
+    void print_string(std::string string_to_print);
 
-  void mouse_click();
+    void mouse_click();
 
-  void mouse_set_coordinate(int x, int y);
+    void mouse_set_coordinate(int x, int y);
 
-  void mouse_set_coordinate(int x, int y, int wheel_pos);
-  void mouse_set_coordinate(int x, int y, int wheel_pos, int pan);
-  void press(uint8_t key) { Keyboard.press(key); }
-  void press_raw(uint8_t key) { Keyboard.pressRaw(key); }
-  void release(uint8_t key) { Keyboard.release(key); }
-  void release_all() { Keyboard.releaseAll(); }
+    void mouse_set_coordinate(int x, int y, int wheel_pos);
+    void mouse_set_coordinate(int x, int y, int wheel_pos, int pan);
+    void press(uint8_t key) { Keyboard.press(key); }
+    void press_raw(uint8_t key) { Keyboard.pressRaw(key); }
+    void release(uint8_t key) { Keyboard.release(key); }
+    void release_all() { Keyboard.releaseAll(); }
+    void begin();
+    void end();
 };
 #endif
